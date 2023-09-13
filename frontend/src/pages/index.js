c9b5eb0cddf854d680d8d1fcfc7517038badbe31
Mark Munyaka <markmunyaka@gmail.com>
2023-09-13 23:12:10 +0000

    Installed and configured Gatsby

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
